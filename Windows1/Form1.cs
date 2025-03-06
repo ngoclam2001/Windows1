8d08d3fc8ec68832fa39508e3e06f495a56344de
NgocLam <nguyenlampv2001@gmail.com>
2025-03-06 19:08:13 +0930

    check

--- Windows1/Form1.cs
+++ Windows1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RazorLight;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,18 +12,32 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Windows1
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             string link = txtLink.Text;
 
-            web_1.CoreWebView2.Navigate("file:///"+link);
+            var engine = new RazorLightEngineBuilder()
+                              .UseEmbeddedResourcesProject(typeof(Form1))
+                              .UseMemoryCachingProvider()
+                              .Build();
+
+            //file .cshtml
+            string template = @"";
+
+            //truyền danh sách
+            var model = new { Name = "World" };
+
+            string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+
+            // Hiển thị lên webview2
+            web_1.NavigateToString(result);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
